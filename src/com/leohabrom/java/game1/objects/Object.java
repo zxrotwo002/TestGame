119aec5f1db4ba06778659f8b790e5932b345f6c
zxrotwo002 <habromleo@gmail.com>
2024-01-28 01:01:40 +0100

    Coins

--- src/com/leohabrom/java/game1/objects/Object.java
+++ src/com/leohabrom/java/game1/objects/Object.java
@@ -7,7 +7,6 @@ import java.awt.image.BufferedImage;
 
 public class Object {
     public BufferedImage image0,image1,image2,image3,image4,image5;
-    public int worldX, worldY;
     Location location = new Location();
 
     public Location getLocation() {
